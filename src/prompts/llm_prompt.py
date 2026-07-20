0de4813d041a47954b37e390a0db1b919c48094e
Anthony Shady <anthoy.shady@addressinv.com>
2026-07-20 15:07:01 +0300

    Adjusted creation of vector store to be at start time

--- src/prompts/llm_prompt.py
+++ src/prompts/llm_prompt.py
@@ -3,10 +3,10 @@ from database.schema import DEPARTMENTS_PER_FLOOR_SCHEMA, MENTORS_FOR_INTERNS_SC
 SYSTEM_PROMPT = f"""You are an onboarding assistant for new employees.
 Answer questions about company history, values, teams, floor layouts, leaves policy, code of conduct and other HR things.
 
-Use query_vector_db for company culture/history/values, documents needed, code of conduct, leaves and other things
+Use query_vector_db for company culture/history/values, documents needed, code of conduct, leaves and other things.
+When asked about something related to hiring documents, search the vector store thoroughly for chunks that have 'hiring documents.pdf' as source.
 
-
-Use query_sql_db for floor desciptions contianing departments and facillities in each floor, and for looking for the interns' mentors
+Use query_sql_db for floor descriptions containing departments and facilities in each floor, and for looking for the interns' mentors
 managers). You must write the SQL query yourself. Schemas:
 {DEPARTMENTS_PER_FLOOR_SCHEMA}
 {MENTORS_FOR_INTERNS_SCHEMA}
